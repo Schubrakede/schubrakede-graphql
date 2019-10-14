cd601bd5de1963ec9fc25bc893362fa98b4e0f90
Lasse Diercks <github@lassediercks.de>
2019-10-14 21:14:36 +0200

    add schema

--- index.js
+++ index.js
@@ -7,12 +7,74 @@ const typeDefs = gql`
   """
   Description
   """
-  type Demo {
+  type Customer {
+    name: String
+    company: Company
+    role: String
+  }
+
+  type Company {
+    name: String
+    amountEmployees: Int
+    adress: String
+  }
+
+  type Schubrakede {
+    workExamples: String
+    workedFor: [Company]
+    primer: String
+    products: [Product]
+    howWeWork: String
+    team: [TeamMember]
+    blog: [Blogarticle]
+    partners: [Partner]
+    legalData: String
+  }
+
+  type Blogarticle {
+    name: String
+    image: String
+    content: String
+    author: Author
+  }
+
+  type Product {
+    name: String
+    duration: String
+    targetGroup: [Company]
+    priceRange: Float
+    isRemote: Boolean
+  }
+
+  type Skill {
     name: String
   }
 
+  type Partner {
+    name: String
+    Image: String
+    description: String
+    link: String
+  }
+
+  type TeamMember {
+    name: String
+    photo: String
+    skills: [Skill]
+    title: String
+    links: String
+  }
+
+  type Author {
+    name: String
+    decription: String
+    twitterlink: String
+  }
+
   type Query {
-    demos: [Demo]
+    customer: [Customer]
+    schubrakede: [Schubrakede]
+    products: [Product]
   }
 `;
 
@@ -90,7 +152,7 @@ const mocks = {
 
 const resolvers = {
   Query: {
-    demos: () => demos
+    customer: () => customer
   }
 };
 
